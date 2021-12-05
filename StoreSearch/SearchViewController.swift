14f0abd1ea916dc6c204b343efe2616b2aa06382
Brang Mai <brangmai100@gmail.com>
2021-12-05 00:30:27 -0500

    Initial commit

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Brang Mai on 12/4/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
