affcd3ae41d4f8f4933d5f1cd36b795cd181809a
Brang Mai <brangmai100@gmail.com>
2021-12-07 00:32:50 -0500

    Add a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Brang Mai on 12/6/21.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
