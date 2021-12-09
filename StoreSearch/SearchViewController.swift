df1dde993da13d9b14692d4dfa0342270e5b5cc3
Brang Mai <brangmai100@gmail.com>
2021-12-09 18:40:59 -0500

    Add searching and displaying results in sorted list

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -24,18 +24,21 @@ class SearchViewController: UIViewController {
 
 // Mark: Search Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
-    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        let searchResult = SearchResult()
-        if searchBar.text! != "justin bieber" {
-            for i in 0...2 {
-                searchResult.name = String(format: "Fake Result %d for", i)
-                searchResult.artistName = searchBar.text!
-                searchResults.append(searchResult)
+        if !searchBar.text!.isEmpty {
+            searchBar.resignFirstResponder()
+            hasSearched = true
+            searchResults = []
+            
+            let url = iTuneURL(searchText: searchBar.text!)
+            print("URL: '\(url)'")
+            if let data = performStoreRequest(with: url) {
+                searchResults = parse(data: data)
+                
+                searchResults.sort { $0.name.localizedStandardCompare($1.name) == .orderedAscending
+                }
             }
         }
-        hasSearched = true
         tableView.reloadData()
     }
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -68,6 +71,11 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             let searchResult = searchResults[indexPath.row]
             cell?.textLabel!.text = searchResult.name
             cell?.detailTextLabel!.text = searchResult.artistName
+//            if searchResult.artist.isEmpty {
+//                cell.artistNameLabel.text = "Unknown"
+//            } else {
+//                cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artist, searchResult.type)
+//            }
         }
     
         return cell!
@@ -83,7 +91,48 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             return indexPath
         }
     }
+
+}
+func iTuneURL(searchText: String) -> URL {
+    let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+    let urlString = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+    let url = URL(string: urlString)
+    return url!
+}
+
+func performStoreRequest(with url: URL) -> Data? {
+    do {
+        return try Data(contentsOf: url)
+    } catch {
+        print("Download Error: \(error.localizedDescription)")
+        shownetworkError()
+        return nil
+    }
+}
+
+func parse(data: Data) -> [SearchResult] {
+    do {
+        let decoder = JSONDecoder()
+        let result = try decoder.decode(ResultArray.self, from: data)
+        return result.results
+    } catch {
+        print("JSON Error: \(error)")
+        return []
+    }
+}
+
+func shownetworkError() {
+    let alert = UIAlertController(title: "Whoops...", message: "There was an error accessing the iTunes Store." + "Please try again.", preferredStyle: .alert)
     
+    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+}
+
+// Source from developer.apple.com
+func present(_ viewControllerToPresent: UIViewController,
+animated flag: Bool,
+completion: (() -> Void)? = nil) {
     
 }
 
